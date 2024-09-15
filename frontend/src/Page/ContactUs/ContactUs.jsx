780b13d9a74a4dc14b2d9492d51efcf1753d9c32
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-09-15 17:30:36 +0530

    update contactUs

--- frontend/src/Page/ContactUs/ContactUs.jsx
+++ frontend/src/Page/ContactUs/ContactUs.jsx
@@ -8,7 +8,7 @@ const ContactUs = () => {
     <main className="pt-28 p-8 w-full font-serif text-center  ">
       <h2 className="text-4xl my-6">Contact Us</h2>
       <div className="flex flex-col  items-center">
-        <div className="mb-10 text-left w-full  ring-1 p-4 ring-black rounded-3xl shadow-black shadow-inner max-w-lg">
+        <div className="mb-10 text-left w-full  ring-1 p-4 ring-slate-300 rounded-3xl shadow-black shadow-inner max-w-lg">
           <h3 className="text-xl mb-4">Contact Information:</h3>
           <p className="mb-2">Email: webops@smail.iitm.ac.in</p>
           <p className="mb-2">Phone: +91-8002860787</p>
@@ -16,7 +16,7 @@ const ContactUs = () => {
             Address: IIT Madras, Chennai, Tamil Nadu, (INDIA),600036
           </p>
         </div>
-        <form className="w-full max-w-lg ring-1 ring-black shadow-black shadow-inner bg-white p-8 rounded-md">
+        <form className="w-full max-w-lg ring-1 ring-slate-300 shadow-black shadow-inner bg-white p-8 rounded-md">
           <div className="mb-4">
             <label
               className="block text-left text-gray-800 text-sm font-bold mb-2"
